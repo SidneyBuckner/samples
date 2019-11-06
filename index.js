f773022f75bd4aceebf56853d1bc855468dab433
sidneybuckner <sidney.buckner2@gmail.com>
2019-11-05 21:04:54 -0600

    add object literals bracket and dot notation

---
+++ index.js
@@ -0,0 +1,13 @@
+// const name = 'Sidney';
+
+// console.log(`Hello, my name is ${name}`);
+
+const me = {
+    'name': 'Sidney',
+    'age': 26,
+    'isSleeping': false,
+
+};
+
+console.log(me['name']);
+console.log(me.age);

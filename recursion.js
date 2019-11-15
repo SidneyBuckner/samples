1d270c1c824a8a8e7b1ef485b63d6019ae599bb1
sidneybuckner <sidney.buckner2@gmail.com>
2019-11-14 19:36:12 -0600

    Show recrusion exit condition

--- recursion.js
+++ recursion.js
@@ -2,13 +2,14 @@
 // Preferably your function DOES NOT use console.log
 // instead it builds/contatonates a long string and return it! Then log in the global scope!
 
-function writePsychoBook(num) {
-  let paragraph = "";
-
-  for (let i = 0; i <= num; i += 1) {
-    paragraph += "All work and no play makes Jack a dull boy. ";
+function writePsychoBook(n) {
+  console.trace();
+  if (n === 0) {
+    return "I am done";
   }
 
-  return paragraph;
+  return `All work and no play makes Jack a dull boy ${writePsychoBook(n - 1)}`;
 }
 console.log(writePsychoBook(99));
+
+// The if statement n === 0 is the exit condition!

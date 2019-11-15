2cdde140997e70be4fd5ad780dd9b45956ef83d9
sidneybuckner <sidney.buckner2@gmail.com>
2019-11-14 19:57:03 -0600

    Replicate math POW

--- recursion.js
+++ recursion.js
@@ -2,14 +2,25 @@
 // Preferably your function DOES NOT use console.log
 // instead it builds/contatonates a long string and return it! Then log in the global scope!
 
-function writePsychoBook(n) {
-  console.trace();
-  if (n === 0) {
-    return "I am done";
+// function writePsychoBook(n) {
+//   console.trace();
+//   if (n === 0) {
+//     return "I am done";
+//   }
+
+//   return `All work and no play makes Jack a dull boy ${writePsychoBook(n - 1)}`;
+// }
+// console.log(writePsychoBook(99));
+
+// // The if statement n === 0 is the exit condition!
+
+function myPow(num, exp) {
+  let ret = num;
+
+  for (let i = 1; i <= exp; i += 1) {
+    ret += num;
   }
 
-  return `All work and no play makes Jack a dull boy ${writePsychoBook(n - 1)}`;
+  return ret;
 }
-console.log(writePsychoBook(99));
-
-// The if statement n === 0 is the exit condition!
+console.log(myPow(2, 3));

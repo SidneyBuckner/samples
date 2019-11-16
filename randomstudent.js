ed683517c7f7bb2e7924e10327ff1d6927ffc056
sidneybuckner <sidney.buckner2@gmail.com>
2019-11-15 21:18:09 -0600

    finish random math app

--- randomstudent.js
+++ randomstudent.js
@@ -13,3 +13,13 @@ const myArr = [
   "Tracie",
   "Wallat"
 ];
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+
+  return Math.floor(Math.random() * (max - min)) + min;
+  // The maximum is exclusive and the minimum is inclusive
+}
+
+console.log(myArr[getRandomInt(0, myArr.length - 1)]);

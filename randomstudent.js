66d905f805c35c19a8556c3c3fd5f5927206b8e8
sidneybuckner <sidney.buckner2@gmail.com>
2019-11-15 21:04:08 -0600

    add randomstudent.js for small app sample

---
+++ randomstudent.js
@@ -0,0 +1,15 @@
+const myArr = [
+  "Andrew",
+  "Billy",
+  "George",
+  "Heather",
+  "Madison",
+  "Marque",
+  "Martin",
+  "Nikki",
+  "Oscar",
+  "RhondaLyn",
+  "Sidney",
+  "Tracie",
+  "Wallat"
+];

c86229478c6a11113ba4edf2a57e8d81cf96a68f
sidneybuckner <sidney.buckner2@gmail.com>
2019-11-21 21:08:19 -0600

    add setter to class.js

--- class.js
+++ class.js
@@ -2,7 +2,7 @@ class Person {
   constructor(first, last, age, occ) {
     this.first = first;
     this.last = last;
-    this, (age = age);
+    this.age = age;
     this.occ = occ;
   }
 
@@ -13,11 +13,15 @@ class Person {
   get fullName() {
     return `${this.first} ${this.last}`;
   }
+  // Setter is set the same way as a getter
+  // The name of the setter MUST BE DIFFERENT from the name of the property!!!
+  set activity(hobby) {
+    this.hobby = this.hobby;
+  }
 }
 const me = new Person("John", "Smith", 35, "Gamer");
 
+// We'll invoke the setter with a snytac that is similar to changing property
 console.log(me.bio);
-
-//
-
-//
+console.log((me.hobby = "reading listerature"));
+console.log(me.bio);

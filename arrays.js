f53d57a50f361f5f80f107ed85b4a2b751e27823
sidneybuckner <sidney.buckner2@gmail.com>
2019-11-21 18:26:12 -0600

    begin array.js lesson and update proptypes

---
+++ arrays.js
@@ -0,0 +1,19 @@
+const numbersAndStrings = [
+  12,
+  "Sid",
+  35,
+  61,
+  "Person",
+  90,
+  "front",
+  "dev",
+  1440,
+  "back"
+];
+
+for (let i = 0; i < numbersAndStrings.length; i += 1) {
+  console.log(numbersAndStrings[i]);
+}
+const popped = numbersAndStrings.pop();
+
+console.log(popped);
---
+++ prototype.js
@@ -0,0 +1,44 @@
+function Profile(first, last, age, occ) {
+  (this.first = first), (this.last = last), (this.age = age), (this.occ = occ);
+
+  this.canDrink = function() {
+    if (this.age < 21) {
+      return "My favorite wine is French. French wine is the best wine.";
+    }
+
+    return "Just kidding, Cali wine is the best.";
+  };
+
+  // Profile.prototype.getHobby = function() {
+  //   if (this.hobby === "Arts & Crafts") {
+  //     return `My favorite hobby is ${this.hobby}`;
+  //   }
+  //   console.error("HOBBY NOT FOUND!!!");
+  // };
+
+  Profile.prototype.getFullName = function() {
+    return `${this.first} ${this.last}`;
+  };
+  Profile.prototype.getBio = function() {
+    return `hi, my name is ${this.getFullName()}. I am ${
+      this.age
+    } years old. ${this.canDrink()}`;
+  };
+}
+const me = new Profile("Sidney", "Buckner", 26, "Software Engineer");
+const cara = new Profile("Cara", "Grimaldi," 28, "Doctor");
+const gale = new Profile("Ashter", "Gale", 29, "Scientist");
+
+them.getFullName = function() {
+  return "No Thanks";
+};
+
+console.log(gale.getFullName());
+console.log(cara.getBio());
+
+
+// console.log(me.getFullName());
+// console.log(me.getBio());
+
+// me.setHobby("Arts and Crafts!");
+// console.log(me);

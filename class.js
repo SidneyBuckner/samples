e0143a2e9d63de91356d8bf625320512abcbc9d4
sidneybuckner <sidney.buckner2@gmail.com>
2019-11-21 21:30:45 -0600

    add more extends and super to class.js

--- class.js
+++ class.js
@@ -22,6 +22,23 @@ class Person {
 const me = new Person("John", "Smith", 35, "Gamer");
 
 // We'll invoke the setter with a snytac that is similar to changing property
-console.log(me.bio);
-console.log((me.hobby = "reading listerature"));
-console.log(me.bio);
+// console.log(me.bio);
+// console.log((me.hobby = "reading listerature"));
+// console.log(me.bio);
+
+// Now we'll inherit the constructor to make another class
+// the extends give access to another class ,
+// super automatically send a call for the information is needs to send down to the new class
+// weve extended employee to be like Person
+
+class Employee extends Person() {
+  constructor(first, last, age, occ) {
+    super(first, last, age, occ);
+    this.id = empId;
+  }
+}
+
+const emp = new Employee("Robert", "Johnson", 20, "tester", "1702957");
+
+console.log(emp);
+console.log(emp.bio);

07f0849508e5bb9fcf94c3be0a81b7b42101cdd1
sidneybuckner <sidney.buckner2@gmail.com>
2019-11-26 21:07:30 -0600

    add shiftUnshitPop.js

---
+++ shiftUnshiftPop.js
@@ -0,0 +1,20 @@
+const fruits = [
+  "apple",
+  "kiwi",
+  "pineapple",
+  "jackfruit",
+  "dragonfruit",
+  "pineapple",
+  "mango",
+  "guava",
+  "strawberry"
+];
+
+const unShifted = fruits.unshift("grapes");
+
+console.log(unShifted);
+
+console.log(fruits);
+
+// push adds to the end of the array
+// method adds one or more elements to the beginning of an array and returns the new length of the array.
